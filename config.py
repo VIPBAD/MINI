270df8b504039efe55036cbdadd7583bb10d9c24
𝚨𝛂ѕͱ꧊𝐢͢к <teamaashik@gmail.com>
2025-08-25 09:09:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ import os
 # Telegram Bot Credentials
 API_ID = int(os.getenv("API_ID", 17609888))  # Replace with your API ID
 API_HASH = os.getenv("API_HASH", "6538740296c179c5f5a2c52f911864d1")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7749179219:AAFex8P31JdTpaoEoKX_EqXXSfCY6eRS7RM")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7259978268:AAH_hNr3Lr65-4R_70-sITMKtVyuU032zRU")
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://mongodbflex:L3GxCeWU3UcgFZZX-hwjiwjve@cluster0.czbo8.mongodb.net/?retryWrites=true&w=majority")
 
 # Log group to receive logs when users start the bot or play songs
-LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", -1002078575375))
+LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", -1002858553810))
 
 # Default YouTube thumbnail if not found
 YT_THUMBNAIL = os.getenv("YT_THUMBNAIL", "https://i.ibb.co/JFr2D1nZ/photo-2025-06-04-08-20-01-7512006038174826500.jpg")

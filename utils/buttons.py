49c398f1fd7aa7cb985b9c977b13ad0ee13cdd1a
VIPBAD <vikramjeetsinghwarval@gmail.com>
2025-09-05 23:09:45 +0530

    Update buttons.py

--- utils/buttons.py
+++ utils/buttons.py
@@ -1,17 +1,23 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from config import MINI_APP_URL, BOT_USERNAME
 
-def get_play_buttons(webapp_url):
+def music_player_buttons(audio_url, title, thumb, chat_id, user_id):
     return InlineKeyboardMarkup([
         [
-            InlineKeyboardButton(
-                text="🎧 Join Room",
-                web_app=WebAppInfo(url=webapp_url)
-            )
+            InlineKeyboardButton("⏭️ Skip", callback_data=f"skip_{chat_id}_{user_id}"),
+            InlineKeyboardButton("⏸️ Pause", callback_data=f"pause_{chat_id}_{user_id}"),
+            InlineKeyboardButton("▶️ Resume", callback_data=f"resume_{chat_id}_{user_id}")
         ],
         [
             InlineKeyboardButton(
-                text="Close",
-                callback_data="close"
-            )
+                text="🎧 Join Room",
+                url=f"https://t.me/{BOT_USERNAME}?startapp={chat_id}"
+            ),
+            InlineKeyboardButton("❌ Close", callback_data=f"close_{chat_id}_{user_id}")
         ]
     ])
+
+def no_more_songs_button():
+    return InlineKeyboardMarkup([[
+        InlineKeyboardButton("🎶 Play New Song", switch_inline_query_current_chat="")
+    ]])
